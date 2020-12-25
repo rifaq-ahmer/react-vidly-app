22cd7fc69efe91a4491134c2b9cc7825efbc41de
rifaq-ahmer <rifaq.ahmer@gmail.com>
2020-12-25 17:06:49 +0530

    till-delete-button

---
+++ src/components/movies.jsx
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import { getMovies } from "../services/fakeMovieService";
+
+class Movies extends Component {
+	state = {
+		movies: getMovies(),
+	};
+	handleDelete = (movie) => {
+		const movies = this.state.movies.filter((m) => m._id !== movie._id);
+		this.setState({ movies });
+	};
+	render() {
+		const { length: count } = this.state.movies;
+		if (count === 0) return <h2>There are no movies in database</h2>;
+		return (
+			<React.Fragment>
+				<p>showing {count} movies in the database </p>
+				<table className="table">
+					<thead>
+						<tr>
+							<th>TITLE</th>
+							<th>GENRE</th>
+							<th>STOCK</th>
+							<th>RATE</th>
+							<th></th>
+						</tr>
+					</thead>
+					<tbody>
+						{this.state.movies.map((movie) => (
+							<tr key={movie._id}>
+								<td>{movie.title}</td>
+								<td>{movie.genre.name}</td>
+								<td>{movie.numberInStock}</td>
+								<td>{movie.dailyRentalRate}</td>
+								<td>
+									<button
+										onClick={() => this.handleDelete(movie)}
+										className="btn btn-danger btn-sm"
+									>
+										DELETE
+									</button>
+								</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
+			</React.Fragment>
+		);
+	}
+}
+
+export default Movies;
