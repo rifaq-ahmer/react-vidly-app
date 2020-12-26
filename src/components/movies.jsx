4ee3c98e38729209178e4c02ed47d52f6e15d9e9
rifaq-ahmer <rifaq.ahmer@gmail.com>
2020-12-26 19:13:25 +0530

    till-like-btn

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { getMovies } from "../services/fakeMovieService";
+import Like from "./common/like";
 
 class Movies extends Component {
 	state = {
@@ -9,6 +10,14 @@ class Movies extends Component {
 		const movies = this.state.movies.filter((m) => m._id !== movie._id);
 		this.setState({ movies });
 	};
+	handleLike = (movie) => {
+		const movies = [...this.state.movies];
+		const index = movies.indexOf(movie);
+		movies[index] = { ...movies[index] };
+		movies[index].liked = !movies[index].liked;
+		this.setState({ movies });
+	};
+
 	render() {
 		const { length: count } = this.state.movies;
 		if (count === 0) return <h2>There are no movies in database</h2>;
@@ -23,6 +32,7 @@ class Movies extends Component {
 							<th>STOCK</th>
 							<th>RATE</th>
 							<th></th>
+							<th></th>
 						</tr>
 					</thead>
 					<tbody>
@@ -32,6 +42,12 @@ class Movies extends Component {
 								<td>{movie.genre.name}</td>
 								<td>{movie.numberInStock}</td>
 								<td>{movie.dailyRentalRate}</td>
+								<td>
+									<Like
+										liked={movie.liked}
+										onClick={() => this.handleLike(movie)}
+									/>
+								</td>
 								<td>
 									<button
 										onClick={() => this.handleDelete(movie)}
